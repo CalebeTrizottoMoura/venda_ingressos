4cffae8f4dbcc90e58c67791c66d41590f9010b8
Calebe Moura <contatocalebemoura@gmail.com>
2023-07-22 14:15:22 -0300

    venda_ingressos_1.0.0

---
+++ README.txt
@@ -0,0 +1,62 @@
+DOCUMENTAÇÃO DO PROGRAMA DE VENDA DE INGRESSOS
+
+Este programa foi desenvolvido em linguagem C e tem o objetivo de 
+realizar vendas de ingressos, listar os ingressos vendidos e 
+validar ingressos através do CPF. O programa utiliza funções 
+básicas de manipulação de strings, estruturas (structs) para 
+armazenar os dados dos clientes, e loops para controlar o fluxo
+do programa.
+
+
+TECNOLOGIAS UTILIZADAS:
+Linguagem de programação C.
+Foram utilizadas as bibliotecas <stdio.h>, <stdlib.h>, 
+<string.h> e <locale.h>.
+
+
+FUNÇÕES UTILIZADAS:
+void clean_buffer(): limpa o buffer do teclado para evitar 
+problemas com caracteres indesejados.
+
+void read_text(char *name, int size): lê uma string digitada 
+pelo usuário (com limite de tamanho) e remove o caractere '\n' 
+que é adicionado pela função fgets.
+
+void format_cpf(char *old_cpf, char new_cpf[]): formata um CPF
+inserindo pontos e hífen.
+
+void vender_ingresso(int *codigo, struct dados_cliente venda[]):
+função para realizar a venda de um ingresso, solicitando o nome
+e CPF do cliente.
+
+void listar_ingressos(int *codigo, struct dados_cliente venda[]):
+lista todos os ingressos vendidos até o momento.
+
+void validar_ingresso(struct dados_cliente venda[]): valida um
+ingresso através do CPF digitado pelo usuário.
+
+
+ESTRUTURAS UTILIZADAS:
+struct dados_cliente: estrutura que armazena os dados do cliente, 
+incluindo nome, CPF e ano de nascimento.
+
+
+COMO UTILIZAR O PROGRAMA:
+Ao executar o programa, você verá um menu principal com as opções 
+disponíveis. Para vender um ingresso, selecione a opção "1" e 
+forneça o nome e CPF do cliente. O CPF será formatado 
+automaticamente. Para listar os ingressos vendidos, selecione 
+a opção "2". Será exibida a lista com nome, CPF e ano de 
+nascimento de cada cliente. Para validar um ingresso, selecione a 
+opção "3" e insira o CPF do cliente. O programa mostrará se o 
+ingresso foi validado ou não. Se desejar sair do programa, 
+selecione a opção "4".
+
+
+OBSERVAÇÕES IMPORTANTES:
+O programa possui um limite de 99 ingressos vendidos. Após atingir 
+esse limite, não será possível vender novos ingressos. Além disso, 
+caso as acentuações, ou outros carecteres especiais, não aparecerem 
+corretamente em seu VS Code, você precisará alterar a codificação 
+padrão para "windows 1252" na opção Encoding. 
+(File > Preferences > Settings > Search Settings > Encoding)
---
+++ venda_ingressos.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAM 99
+
+void clean_buffer(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF){}
+}
+
+void read_text(char *name, int size){
+    fgets(name, size, stdin);
+	strtok(name, "\n");
+}
+
+void format_cpf(char *old_cpf, char new_cpf[]){
+	sprintf(new_cpf, "%.3s.%.3s.%.3s-%.2s", old_cpf, old_cpf + 3, old_cpf + 6, old_cpf + 9);
+}
+
+struct dados_cliente{
+	char nome[50];
+	char cpf[15];
+	int nasc;
+};
+
+void vender_ingresso(int *codigo, struct dados_cliente venda[]){
+	char digit_cpf[15], new_cpf[15];
+	int cont;
+
+	printf("====== VENDA DE INGRESSOS ======\n");
+	printf("Nome: ");
+	read_text(venda[*codigo].nome, 50);
+
+	do{
+		cont = 0;
+		printf("CPF: ");
+		read_text(digit_cpf, 15);
+		format_cpf(digit_cpf, new_cpf);
+
+		for(int i = 0; i < TAM; i++){
+			if(strcmp(new_cpf, venda[i].cpf) == 0){
+				printf("Um ingresso já foi vendido para esse CPF.\n");
+				printf("Por favor, digite outro CPF válido.\n");
+				cont++;
+			}
+		}
+
+		if(strlen(new_cpf) != 14){
+			printf("Por favor, digite um CPF com 11 dígitos.\n");
+			cont++;
+		}
+	}while(cont != 0);
+	strcpy(venda[*codigo].cpf, new_cpf);
+
+	printf("Ano de nascimento: ");
+	scanf("%d", &venda[*codigo].nasc);
+	clean_buffer();
+	
+	system("cls");
+	printf("====== VENDA DE INGRESSOS ======\n");
+	printf("Venda realizada com sucesso!\n\n");
+	system("pause");
+}
+
+void listar_ingressos(int *codigo, struct dados_cliente venda[]){
+	printf("==== LISTAGEM DE INGRESSOS ====\n");
+	
+	for(int i = 0; i <= TAM; i++){
+		if (i <= *codigo - 1){
+			printf("Nome: %s\n", venda[i].nome);
+			printf("CPF: %s\n", venda[i].cpf);
+			printf("Ano de nascimento: %d\n", venda[i].nasc);
+			printf("===============================\n\n");
+		}
+	}
+	
+	system("pause");
+}
+
+void validar_ingresso(struct dados_cliente venda[]){
+	char validar[15], convert[15];
+	int controle;
+	
+	printf("====== VALIDAÇÃO DE INGRESSOS ======\n");
+	printf("Digite o CPF para validação: ");
+	read_text(convert, 15);
+	format_cpf(convert, validar);
+	
+	system("cls");
+	
+	for(int i = 0; i <= TAM; i++){
+		if (strcmp(venda[i].cpf, validar) == 0){
+			printf("====== INGRESSO VALIDADO ======\n");
+			printf("Nome: %s\n", venda[i].nome);
+			printf("CPF: %s\n", venda[i].cpf);
+			printf("Ano de nascimento: %d\n", venda[i].nasc);
+			printf("===============================\n\n");
+			controle = 1;
+		}
+	}
+	
+	if(controle != 1){
+		printf("==== INGRESSO NÃO VALIDADO ====\n\n");
+	}
+		
+	system("pause");
+}
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	int x = 0, codigo = 0;
+	struct dados_cliente venda[TAM];	
+	
+	do{
+		printf("========= MENU PRINCIPAL =========\n");
+		printf("1 - Vender ingresso.\n");
+		printf("2 - Listar ingressos vendidos.\n");
+		printf("3 - Validar ingresso.\n");
+		printf("4 - Sair.\n");
+		printf("\nDigite a opção desejada: ");
+		scanf("%d", &x);
+		clean_buffer();
+		system("cls");
+		
+		switch(x){
+			case 1: 
+				vender_ingresso(&codigo, venda);
+				codigo = codigo + 1;
+				break;
+			case 2: 
+				listar_ingressos(&codigo, venda);
+				break;
+			case 3: 
+				validar_ingresso(venda);
+				break;
+			case 4: 
+				printf("=========== SAINDO ===========\n\n");
+				system("pause");
+				break;
+			default: 
+				printf("======= OPÇÃO INVÁLIDA =======\n\n");
+				system("pause");
+				break;
+		}
+		
+		system("cls");
+	}while(x != 4);
+	
+	return 0;
+}
